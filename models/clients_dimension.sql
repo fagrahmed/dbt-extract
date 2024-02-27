fbd9ef089571ad1ae6e92f1cc1b2bfafa20bb913
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-02-27 12:13:12 +0200

    add "client_modifiedat" to clients_dimension.sql

--- models/clients_dimension.sql
+++ models/clients_dimension.sql
@@ -8,6 +8,7 @@ SELECT
     clientname->>'en' as clientname_en,
     clienttype,
     (createdat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS client_createdat_utc2,
+    (lastmodifiedat::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS client_modifiedat_utc2,
 
     status as client_status,
     industrytype,
