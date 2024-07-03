f85600e56c1fd6a12b0e1eea67e8a3a99df899ca
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-07-03 17:30:22 +0300

    Update and rename employees_dimension.sql to inc_employees_dimension.sql
    changed all sources/destinations to inc_(name)

--- models/inc_employees_dimension.sql
+++ models/inc_employees_dimension.sql
@@ -6,22 +6,22 @@
         unique_key= "hash_column",
         on_schema_change='append_new_columns',
         post_hook="
-            DROP TABLE IF EXISTS employees_stagging;
-            DROP TABLE IF EXISTS employees_stagging_2;
+            DROP TABLE IF EXISTS inc_employees_stagging;
+            DROP TABLE IF EXISTS inc_employees_stagging_2;
             "
     )
 }}
 
 with step_1 as (
     select stg.*
-    from {{ ref("employees_stagging") }} stg 
-    left join {{ ref("employees_stagging_2")}} stg2 on stg.hash_column = stg2.hash_column
+    from {{ ref("inc_employees_stagging") }} stg 
+    left join {{ ref("inc_employees_stagging_2")}} stg2 on stg.hash_column = stg2.hash_column
     where stg2.hash_column is null
 
     union 
 
     select *
-    from {{ref("employees_stagging_2")}}
+    from {{ref("inc_employees_stagging_2")}}
 )
 
 {%if is_incremental() %}
