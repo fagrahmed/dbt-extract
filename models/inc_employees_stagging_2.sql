e7f59d6f5a9b79f74b60641173219aacfdc48017
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-07-03 17:27:45 +0300

    Update and rename employees_stagging_2.sql to inc_employees_stagging_2.sql
    changed all sources/destinations to inc_(name)

--- models/inc_employees_stagging_2.sql
+++ models/inc_employees_stagging_2.sql
@@ -5,7 +5,7 @@
     on_schema_change='create'
 )}}
 
-{% set table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'employees_dimension')" %}
+{% set table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'inc_employees_dimension')" %}
 {% set table_exists_result = run_query(table_exists_query) %}
 {% set table_exists = table_exists_result.rows[0][0] if table_exists_result and table_exists_result.rows else False %}
 
@@ -81,8 +81,8 @@ with update_old as (
 
         (now()::timestamptz AT TIME ZONE 'UTC' + INTERVAL '2 hours') AS loaddate  
 
-    FROM {{ source('dbt-dimensions', 'employees_stagging') }} stg
-    LEFT JOIN {{ source('dbt-dimensions', 'employees_dimension')}} final
+    FROM {{ source('dbt-dimensions', 'inc_employees_stagging') }} stg
+    LEFT JOIN {{ source('dbt-dimensions', 'inc_employees_dimension')}} final
         ON stg.employee_id = final.employee_id AND stg.employee_mobile = final.employee_mobile
     WHERE final.hash_column is not null and final.operation != 'exp'
         AND stg.loaddate > final.loaddate
@@ -95,7 +95,7 @@ SELECT * from update_old
 -- Do nothing in first load
 SELECT *
 
-FROM {{ source('dbt-dimensions', 'employees_stagging') }} stg
+FROM {{ source('dbt-dimensions', 'inc_employees_stagging') }} stg
 WHERE stg.loaddate > '2050-01-01'::timestamptz
 
 {% endif %}
