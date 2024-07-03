88c43502ad577bb83bbd818a548de958f19c33b6
Fagr Ahmed <60551211+fagrahmed@users.noreply.github.com>
2024-07-03 17:24:37 +0300

    Update and rename clients_stagging.sql to inc_clients_stagging.sql
    changed all sources/destinations to inc_(name)

--- models/inc_clients_stagging.sql
+++ models/inc_clients_stagging.sql
@@ -5,11 +5,11 @@
     on_schema_change='append_new_columns'
 )}}
 
-{% set table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'clients_dimension')" %}
+{% set table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'inc_clients_dimension')" %}
 {% set table_exists_result = run_query(table_exists_query) %}
 {% set table_exists = table_exists_result.rows[0][0] if table_exists_result and table_exists_result.rows else False %}
 
-{% set stg_table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'clients_stagging')" %}
+{% set stg_table_exists_query = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'dbt-dimensions' AND table_name = 'inc_clients_stagging')" %}
 {% set stg_table_exists_result = run_query(stg_table_exists_query) %}
 {% set stg_table_exists = stg_table_exists_result.rows[0][0] if stg_table_exists_result and stg_table_exists_result.rows else False %}
 
@@ -42,5 +42,5 @@ SELECT
 FROM {{source('axis_sme', 'clients') }} src
 
 {% if is_incremental() and table_exists and stg_table_exists %}
-    WHERE src._airbyte_emitted_at > COALESCE((SELECT max(loaddate::timestamptz) FROM {{ source('dbt-dimensions', 'clients_dimension') }}), '1900-01-01'::timestamp)
+    WHERE src._airbyte_emitted_at > COALESCE((SELECT max(loaddate::timestamptz) FROM {{ source('dbt-dimensions', 'inc_clients_dimension') }}), '1900-01-01'::timestamp)
 {% endif %}
